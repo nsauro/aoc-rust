d9ab84d8c66b64c79076109ced7aaef63b0661ee
Nick Sauro <nick.sauro@gmail.com>
2024-01-05 08:55:33 -0500

    fmt

--- 2015/src/bin/day_03/day_03.rs
+++ 2015/src/bin/day_03/day_03.rs
@@ -1,5 +1,5 @@
-use std::fs;
 use std::collections::HashMap;
+use std::fs;
 fn main() {
     let input = fs::read_to_string("src/bin/day_03/input.data").unwrap();
     let part1_res = part1(&input);
@@ -9,37 +9,41 @@ fn main() {
     println!("part 2 result: {}", part2_res);
 
     fn part1(s: &String) -> i32 {
+        let (locs, _) = s
+            .chars()
+            .fold((HashMap::new(), (0, 0)), |(mut acc, cur_loc), c| {
+                let new_dir = get_next(cur_loc, c);
+                let count = acc.entry(new_dir).or_insert(0);
+                *count += 1;
+                (acc, new_dir)
+            });
 
-        let (locs, _) = s.chars().fold((HashMap::new(), (0,0)), |(mut acc, cur_loc), c| {
-            let new_dir = get_next(cur_loc, c);
-            let count = acc.entry(new_dir).or_insert(0);
-            *count += 1;
-            (acc, new_dir)
-        });
-
-        let x = locs.iter().filter(|&(_, &value)|  value >= 1);
+        let x = locs.iter().filter(|&(_, &value)| value >= 1);
         x.count() as i32
     }
 
     fn part2(s: &String) -> i32 {
-        let (locs, _) = s.chars().fold((HashMap::new(), ((0,0), (0,0))), |(mut acc, (cur_loc, other_loc)), c| {
-            let new_dir = get_next(cur_loc, c) ;
-            let count = acc.entry(new_dir).or_insert(0);
-            *count += 1;
-            (acc, (other_loc, new_dir))
-        });
+        let (locs, _) = s.chars().fold(
+            (HashMap::new(), ((0, 0), (0, 0))),
+            |(mut acc, (cur_loc, other_loc)), c| {
+                let new_dir = get_next(cur_loc, c);
+                let count = acc.entry(new_dir).or_insert(0);
+                *count += 1;
+                (acc, (other_loc, new_dir))
+            },
+        );
 
-        let x = locs.iter().filter(|&(_, &value)|  value >= 1);
+        let x = locs.iter().filter(|&(_, &value)| value >= 1);
         x.count() as i32
     }
 
-    fn get_next(cur_loc : (i32, i32), c: char) -> (i32, i32) {
+    fn get_next(cur_loc: (i32, i32), c: char) -> (i32, i32) {
         match c {
             '^' => (cur_loc.0 - 1, cur_loc.1),
             '<' => (cur_loc.0, cur_loc.1 - 1),
             'v' => (cur_loc.0 + 1, cur_loc.1),
             '>' => (cur_loc.0, cur_loc.1 + 1),
-            _   => cur_loc
+            _ => cur_loc,
         }
     }
 }
