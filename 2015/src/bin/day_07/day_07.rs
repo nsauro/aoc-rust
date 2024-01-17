e9d9285b57d08dd2d50e2ced0520f840de3cf12e
Nick Sauro <nick.sauro@gmail.com>
2024-01-16 21:31:37 -0500

    formatting

--- 2015/src/bin/day_07/day_07.rs
+++ 2015/src/bin/day_07/day_07.rs
@@ -1,111 +1,108 @@
+use once_cell::sync::Lazy;
+use regex::Regex;
 use std::collections::HashMap;
 use std::fs;
-use regex::Regex;
-use once_cell::sync::Lazy;
 
-static SET_VALUE: Lazy<Regex> =  Lazy::new(|| Regex::new(r"^(\d+) -> ([a-z]+)$").unwrap());
+static SET_VALUE: Lazy<Regex> = Lazy::new(|| Regex::new(r"^(\d+) -> ([a-z]+)$").unwrap());
 static COPY_VALUE: Lazy<Regex> = Lazy::new(|| Regex::new(r"^([a-z]+) -> ([a-z]+)$").unwrap());
 static AND: Lazy<Regex> = Lazy::new(|| Regex::new(r"^([a-z]+) AND ([a-z]+) -> ([a-z]+)$").unwrap());
 static AND_1: Lazy<Regex> = Lazy::new(|| Regex::new(r"^1 AND ([a-z]+) -> ([a-z]+)$").unwrap());
 static OR: Lazy<Regex> = Lazy::new(|| Regex::new(r"^([a-z]+) OR ([a-z]+) -> ([a-z]+)$").unwrap());
-static LSHIFT: Lazy<Regex> = Lazy::new(|| Regex::new(r"^([a-z]+) LSHIFT (\d+) -> ([a-z]+)$").unwrap());
-static  RSHIFT: Lazy<Regex> = Lazy::new(|| Regex::new(r"(^[a-z]+) RSHIFT (\d+) -> ([a-z]+)$").unwrap());
+static LSHIFT: Lazy<Regex> =
+    Lazy::new(|| Regex::new(r"^([a-z]+) LSHIFT (\d+) -> ([a-z]+)$").unwrap());
+static RSHIFT: Lazy<Regex> =
+    Lazy::new(|| Regex::new(r"(^[a-z]+) RSHIFT (\d+) -> ([a-z]+)$").unwrap());
 static NOT: Lazy<Regex> = Lazy::new(|| Regex::new(r"NOT ([a-z]+) -> ([a-z]+)").unwrap());
 fn main() {
-
     let raw = fs::read_to_string("src/bin/day_07/input.data").unwrap();
     //note: data here is updated for part 2 with the result of part 1! recopy from source
 
     let input_1 = raw.lines();
 
-    let mut map_1 : HashMap<String, u32> = HashMap::new();
+    let mut map_1: HashMap<String, u32> = HashMap::new();
     process(&mut map_1, input_1.clone().map(|i| i.to_string()).collect());
 
     println!("part1: {}", map_1.get("a").unwrap());
     println!("part1: {}", map_1.get("b").unwrap());
-
 }
 
-fn process(registers: &mut HashMap<String, u32>, ins: Vec<String> ) {
+fn process(registers: &mut HashMap<String, u32>, ins: Vec<String>) {
     let mut next: Vec<String> = Vec::new();
     println!("processing {} instructions", ins.len());
-    ins.iter().for_each( |i| {
-        if let Some(c) = SET_VALUE.captures(i){
+    ins.iter().for_each(|i| {
+        if let Some(c) = SET_VALUE.captures(i) {
             println!("doing set value {}", i);
             let key = c.get(2).unwrap().as_str().to_string();
             let value = c.get(1).unwrap().as_str().parse::<u32>().unwrap();
             registers.insert(key, value);
-
-        } else if let Some(c) = COPY_VALUE.captures(i){
+        } else if let Some(c) = COPY_VALUE.captures(i) {
             let s = c.get(1).unwrap().as_str();
             let d = c.get(2).unwrap().as_str();
-            if let Some(v) = registers.get(s){
+            if let Some(v) = registers.get(s) {
                 println!("executing copy {}", i);
                 registers.insert(d.to_string(), *v);
-            }else{
+            } else {
                 next.push(i.to_string());
             }
         } else if let Some(c) = AND.captures(i) {
             let f = c.get(1).unwrap().as_str();
             let s = c.get(2).unwrap().as_str();
             let d = c.get(3).unwrap().as_str();
-            if let (Some(v1), Some(v2)) = (registers.get(f), registers.get(s)){
+            if let (Some(v1), Some(v2)) = (registers.get(f), registers.get(s)) {
                 println!("executing and {}", i);
                 registers.insert(d.to_string(), v1 & v2);
-            }else{
+            } else {
                 next.push(i.to_string());
             }
-
-        }else if let Some(c) = AND_1.captures(i) {
+        } else if let Some(c) = AND_1.captures(i) {
             let s = c.get(1).unwrap().as_str();
             let d = c.get(2).unwrap().as_str();
-            if let Some(v) = registers.get(s){
+            if let Some(v) = registers.get(s) {
                 println!("executing and 1  {}", i);
                 registers.insert(d.to_string(), 1 & v);
-            }else{
+            } else {
                 next.push(i.to_string());
             }
-        }else if let Some(c) = OR.captures(i){
+        } else if let Some(c) = OR.captures(i) {
             let f = c.get(1).unwrap().as_str();
             let s = c.get(2).unwrap().as_str();
             let d = c.get(3).unwrap().as_str();
-            if let (Some(v1), Some(v2)) = (registers.get(f), registers.get(s)){
+            if let (Some(v1), Some(v2)) = (registers.get(f), registers.get(s)) {
                 println!("executing or {}", i);
                 registers.insert(d.to_string(), v1 | v2);
-            }else{
+            } else {
                 next.push(i.to_string());
             }
-        }else if let Some(c) = LSHIFT.captures(i){
+        } else if let Some(c) = LSHIFT.captures(i) {
             let s = c.get(1).unwrap().as_str();
             let amount = c.get(2).unwrap().as_str().parse::<u32>().unwrap();
             let d = c.get(3).unwrap().as_str();
-            if let Some(v) = registers.get(s){
+            if let Some(v) = registers.get(s) {
                 println!("executing lshift {}", i);
                 registers.insert(d.to_string(), v << amount);
-            }else{
+            } else {
                 next.push(i.to_string());
             }
-        }else if let Some(c) = RSHIFT.captures(i){
+        } else if let Some(c) = RSHIFT.captures(i) {
             let s = c.get(1).unwrap().as_str();
             let amount = c.get(2).unwrap().as_str().parse::<u32>().unwrap();
             let d = c.get(3).unwrap().as_str();
-            if let Some(v) = registers.get(s){
+            if let Some(v) = registers.get(s) {
                 println!("executing rshift {}", i);
                 registers.insert(d.to_string(), v >> amount);
-            }else{
+            } else {
                 next.push(i.to_string());
             }
-
-        }else if let Some(c) = NOT.captures(i){
+        } else if let Some(c) = NOT.captures(i) {
             let s = c.get(1).unwrap().as_str();
             let d = c.get(2).unwrap().as_str();
-            if let Some(v) = registers.get(s){
+            if let Some(v) = registers.get(s) {
                 println!("executing not {}", i);
                 registers.insert(d.to_string(), !v);
-            }else{
+            } else {
                 next.push(i.to_string());
             }
-        }else{
+        } else {
             panic!("omg!!!! {}", i)
         }
     });
--- 2015/src/bin/day_08/day_08.rs
+++ 2015/src/bin/day_08/day_08.rs
@@ -1,16 +1,15 @@
 use std::fs;
 
 fn main() {
-
     let raw = fs::read_to_string("src/bin/day_08/input.data").unwrap();
 
     let (total_size, total_chars) = &raw.lines().fold((0u32, 0u32), |(size, chars), s| {
         (size + s.len() as u32, chars + get_chars(s)) //safe b/c these sizes are small
     });
 
-    let new_encoded_size = raw.lines().fold(0u32, |size, s| {
-        size + get_encoded_length(s)
-    });
+    let new_encoded_size = raw
+        .lines()
+        .fold(0u32, |size, s| size + get_encoded_length(s));
 
     let part1_ans = total_size - total_chars;
     println!("part 1: {}", part1_ans);
@@ -19,38 +18,36 @@ fn main() {
     println!("part 2: {}", part2_ans);
 }
 
-fn get_chars(s : &str) -> u32 {
+fn get_chars(s: &str) -> u32 {
     let mut count = 0;
     let mut iter = s.chars().into_iter();
-    while let Some(c) = iter.next(){
+    while let Some(c) = iter.next() {
         match c {
             '"' => (),
-            '\\' => {
-                match iter.next().unwrap(){
-                    '"' => count += 1,
-                    '\\' => count += 1,
-                    'x' => {
-                        count += 1;
-                        iter.next();
-                        iter.next();
-                    }
-                    _  => panic!("omg!!")
+            '\\' => match iter.next().unwrap() {
+                '"' => count += 1,
+                '\\' => count += 1,
+                'x' => {
+                    count += 1;
+                    iter.next();
+                    iter.next();
                 }
-            }  ,
-            _ => count += 1
+                _ => panic!("omg!!"),
+            },
+            _ => count += 1,
         }
     }
     count
 }
 
-fn get_encoded_length(s : &str) -> u32 {
+fn get_encoded_length(s: &str) -> u32 {
     let mut count = 0;
     let mut iter = s.chars().into_iter();
-    while let Some(c) = iter.next(){
+    while let Some(c) = iter.next() {
         match c {
-            '"' => count += 2, //needs needs encoding
+            '"' => count += 2,  //needs needs encoding
             '\\' => count += 2, //needs encoding
-            _ => count += 1
+            _ => count += 1,
         }
     }
     count + 2
